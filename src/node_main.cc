56095479856ab53d37f0fe51a573c2aec5ef5d43
yuzewen <1979933731@qq.com>
2021-10-08 10:00:06 +0800

    overall design

---
+++ src/node_main.cc
@@ -0,0 +1,9 @@
+//1. Read the Config file
+// 
+//2. Listen the node reconstruct the failed node
+//3. Send the node information to repair node
+//4. If the node don't failed, send date it stored to the repair node
+// 读取配置文件
+// 在节点创建一个随机文件
+// 监听修复节点（目标节点）的连接请求
+// 如果该节点没有失效，将该节点中储存的数据传输到目标节点
